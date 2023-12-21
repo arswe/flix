cee9625ec4989348012a2fdf363ffe8d15ae641b
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-21 22:23:47 +0600

    make some update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatGridListModule } from '@angular/material/grid-list';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatGridListModule, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'flix';
-
-  func() {
-    console.log('func');
-  }
-}
+export class AppComponent {}
