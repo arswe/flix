8ab07cc189602446c7344e95088a88b9a356b4f9
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-22 15:32:01 +0600

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,3 @@
 import { Routes } from '@angular/router';
-import { AboutComponent } from './about/about.component';
-import { ContactsComponent } from './contacts/contacts.component';
-import { HomeComponent } from './home/home.component';
-import { ProductsComponent } from './products/products.component';
 
-export const routes: Routes = [
-  { path: '', title: 'Home', component: HomeComponent },
-  { path: 'about', title: 'about', component: AboutComponent },
-  { path: 'products', title: 'products', component: ProductsComponent },
-  { path: 'contact', title: 'contact', component: ContactsComponent },
-];
+export const routes: Routes = [];
