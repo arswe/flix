6249396fbb1a5f699734cd3cd851348898aed7d5
abdurrahmansoftw <abdurrahmansoftw@gmail.com>
2023-12-23 13:49:34 +0600

    practices

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,8 +5,9 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
-
+  title = 'app';
+  logoName = 'AbdurRahman';
 }
