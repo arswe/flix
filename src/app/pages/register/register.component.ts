10bd49e9d5f94f3a198a69289d72954b48f98eab
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-14 15:24:34 +0600

    Register add

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CardComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss',
 })
