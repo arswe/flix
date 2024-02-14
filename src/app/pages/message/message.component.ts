85edc714f3cf81d3f33df72ec8444a744ed7b23f
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-14 15:34:30 +0600

    compose and message

--- src/app/pages/message/message.component.ts
+++ src/app/pages/message/message.component.ts
@@ -1,9 +1,12 @@
-import { Component } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { CardComponent } from '../../components/card/card.component';
+import { SidebarComponent } from '../../layouts/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [CardComponent, SidebarComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss',
 })
