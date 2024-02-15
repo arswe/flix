93b303b9c441cee20ee2da352e5f45cd4731af31
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 19:29:34 +0600

    AuthController login

--- backend/controllers/AuthCOntroller.ts
+++ backend/controllers/AuthCOntroller.ts
@@ -1,7 +1,11 @@
 import { Request, Response } from 'express';
 
-class AuthCOntroller {
-  public async login(request: Request, response: Response) {}
+class AuthController {
+  public async login(request: Request, response: Response) {
+    return response.json({ message: 'login' });
+  }
   public async register(request: Request, response: Response) {}
   public async logout(request: Request, response: Response) {}
 }
+
+export default new AuthController();
