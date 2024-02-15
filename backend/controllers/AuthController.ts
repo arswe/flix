93440cba9789791e448b9ac98b3d7512f42c5f12
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 19:46:17 +0600

    register  AuthController

--- backend/controllers/AuthController.ts
+++ backend/controllers/AuthController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express';
+import { registerSchema } from '../validator';
 
 class AuthController {
   public async login(request: Request, response: Response) {
     return response.json({ message: 'login' });
   }
-  public async register(request: Request, response: Response) {}
+  public async register(request: Request, response: Response) {
+    try {
+      const { name, email, password } = request.body;
+      registerSchema.parse({ name, email, password });
+    } catch (error) {
+      return response.status(400).json({ status: false, error });
+    }
+  }
   public async logout(request: Request, response: Response) {}
 }
 
