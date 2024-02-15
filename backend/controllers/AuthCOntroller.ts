4e55d7abf4ca7996346b77b70bc5cf2240f689b7
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 19:28:39 +0600

    AuthCOntroller

---
+++ backend/controllers/AuthCOntroller.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+class AuthCOntroller {
+  public async login(request: Request, response: Response) {}
+  public async register(request: Request, response: Response) {}
+  public async logout(request: Request, response: Response) {}
+}
