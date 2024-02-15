a6ce50fbf0b0259b2c8b122ddf62881492a135b9
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 19:11:38 +0600

    app

--- backend/app.ts
+++ backend/app.ts
@@ -1,14 +1,19 @@
 import colors from 'colors';
+import dotenv from 'dotenv';
 import express from 'express';
+
+dotenv.config();
+const port = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.json());
 
-const PORT = 8000;
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+  res.send('hello');
+});
+
+app.listen(port, () => {
   console.log(
-    colors.rainbow(
-      `Server is running on port http://localhost:${process.env.PORT}`
-    )
+    colors.rainbow(`Server is running on port http://localhost:${port}`)
   );
 });
