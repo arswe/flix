a7955fe122d965fcdd96403bf74b4573b098a612
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 20:40:26 +0600

    splittedToken

--- backend/middleware/authenticate.ts
+++ backend/middleware/authenticate.ts
@@ -21,6 +21,15 @@ export const validateUser = async (
     const splittedToken = authorization.split(' ');
 
     try {
+      try {
+        const token = splittedToken[1];
+        const user: IUser = (await verify(token, SECRETE_KEY)) as IUser;
+        const isValidToken = await isTokenValid(user.email, token);
+  
+        if (user && isValidToken) {
+          merge(request, { user });
+          return next();
+        }
     } catch (error) {
       return response
         .status(401)
