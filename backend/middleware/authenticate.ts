43c5dfc551ac42408a4bc19c2249adf6f60d94ff
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-15 20:27:22 +0600

    generateToken

--- backend/middleware/authenticate.ts
+++ backend/middleware/authenticate.ts
@@ -1,7 +1,9 @@
 import { sign } from 'jsonwebtoken';
+import { IUser } from '../models/common.model';
 
+const SECRETE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9';
 
-export const generateToken = (data) => {
+export const generateToken = (data: IUser) => {
   const token = sign(data, SECRETE_KEY, {
     expiresIn: '1 day',
   });
