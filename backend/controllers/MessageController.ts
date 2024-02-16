fa20584b88c0c143a9bf597fb658c5d8ded147b6
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 11:14:08 +0600

    getAllMessage

---
+++ backend/controllers/MessageController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+class MessageController {
+  public async getAllMessage(request: Request, response: Response) {}
+}
+
+export default new MessageController();
