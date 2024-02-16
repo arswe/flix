1ca0fb04f0c8da8efd04824f757f43adfece121f
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 13:30:28 +0600

    getAllUsers

--- backend/routes/route.ts
+++ backend/routes/route.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import AuthController from '../controllers/AuthController';
 import MessageController from '../controllers/MessageController';
+import UserController from '../controllers/UserController';
 import { validateUser } from '../middleware/authenticate';
 
 const router = express.Router();
@@ -10,10 +11,16 @@ router.post('/auth/register', AuthController.register);
 router.get('/auth/me', validateUser, AuthController.me);
 router.get('/auth/logout', validateUser, AuthController.logout);
 
+router.get('/users', validateUser, UserController.getAllUsers);
+
 router.get('/message', validateUser, MessageController.getAllMessage);
 router.get('/message/:id', validateUser, MessageController.getMessage);
 router.get('/message/:id/read', validateUser, MessageController.readMessage);
-router.get('/message/:id/delete', validateUser, MessageController.deleteMessage);
+router.get(
+  '/message/:id/delete',
+  validateUser,
+  MessageController.deleteMessage
+);
 router.post('/message', validateUser, MessageController.createMessage);
 
 export default router;
