9a042007647ddd54c6e8938e80ed3c875a801018
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 11:18:04 +0600

    getMessage

--- backend/controllers/MessageController.ts
+++ backend/controllers/MessageController.ts
@@ -11,6 +11,16 @@ class MessageController {
       return response.status(400).json({ status: false, error });
     }
   }
+  public async getMessage(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+
+      const message = await MessageModel.findById(id);
+      return response.status(201).json({ data: message });
+    } catch (error) {
+      return response.status(400).json({ status: false, error });
+    }
+  }
 }
 
 export default new MessageController();
