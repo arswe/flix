bd7a69fc83fae673baf84f76209e28c6c3e15500
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 13:41:12 +0600

    interface ILoginRespose

---
+++ src/app/core/models/auth.model.ts
@@ -0,0 +1,17 @@
+export interface IUser {
+  _id: string;
+  name: string;
+  email: string;
+  token: string;
+}
+
+export interface IEmail {
+  email: string;
+  password: string;
+}
+
+export interface ILoginRespose {
+  message: string;
+  token: string;
+  user: IUser;
+}
--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor() {}
 
-  constructor() { }
+  login() {}
+  logout() {}
 }
