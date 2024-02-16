72f1cf881b2a06aee5b361210ca6c97768559bfa
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 13:15:22 +0600

    add message route

--- backend/routes/route.ts
+++ backend/routes/route.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import AuthController from '../controllers/AuthController';
+import MessageController from '../controllers/MessageController';
 import { validateUser } from '../middleware/authenticate';
 
 const router = express.Router();
@@ -8,5 +9,7 @@ router.post('/auth/login', AuthController.login);
 router.post('/auth/register', AuthController.register);
 router.get('/auth/me', validateUser, AuthController.me);
 router.get('/auth/logout', validateUser, AuthController.logout);
+
+router.post('/message', validateUser, MessageController.createMessage);
 
 export default router;
