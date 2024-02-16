740b68aa40dcdd2b494084825b33f9a473cab16a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 11:20:12 +0600

    messageSchema

--- backend/validator/index.ts
+++ backend/validator/index.ts
@@ -2,15 +2,22 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
   email: z
-    .string({ required_error: 'Name is required' })
+    .string({ required_error: 'Email is required' })
     .email('Invalid Email Address'),
-  password: z.string({ required_error: 'password is required' }).min(6),
+  password: z.string({ required_error: 'Password is required' }),
 });
 
 export const registerSchema = z.object({
-  name: z.string({ required_error: 'Name is required' }).min(2),
+  name: z.string({ required_error: 'Name is required' }).min(3),
   email: z
-    .string({ required_error: 'Name is required' })
+    .string({ required_error: 'Email is required' })
     .email('Invalid Email Address'),
-  password: z.string({ required_error: 'password is required' }).min(6),
+  password: z.string({ required_error: 'Password is required' }).min(6),
+});
+
+export const messageSchema = z.object({
+  to: z.string({ required_error: 'To is required' }),
+  sender: z.string({ required_error: 'Sender is required' }),
+  subject: z.string({ required_error: 'Subject is required' }),
+  body: z.string({ required_error: 'Body is required' }),
 });
