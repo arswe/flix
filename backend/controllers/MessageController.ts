5a9e6492ebf7e32471a28b143a19a367b2aa8557
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-16 11:15:57 +0600

    message find

--- backend/controllers/MessageController.ts
+++ backend/controllers/MessageController.ts
@@ -1,7 +1,13 @@
 import { Request, Response } from 'express';
+import { MessageModel } from '../models/message';
 
 class MessageController {
-  public async getAllMessage(request: Request, response: Response) {}
+  public async getAllMessage(request: Request, response: Response) {
+    try {
+      const message = await MessageModel.find();
+      
+    } catch (error) {}
+  }
 }
 
 export default new MessageController();
