f2872995b4f4812ce631304ba64b2ea62f067049
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-17 18:26:51 +0600

    tokenService

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,9 +1,18 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { TokenService } from './../services/token.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  tokenService.isAuthentication.subscribe({
+    next(value) {
+      if (value) {
+        router.navigate(['message']);
+      }
+    },
+  });
 
   return true;
 };
