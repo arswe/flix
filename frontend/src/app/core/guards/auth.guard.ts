1cc89cedb802cfc824182ab05c6b1411cc619da6
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-17 18:24:40 +0600

    authGuard

--- frontend/src/app/core/guards/auth.guard.ts
+++ frontend/src/app/core/guards/auth.guard.ts
@@ -1,5 +1,9 @@
+import { inject } from '@angular/core';
 import { CanActivateFn } from '@angular/router';
+import { TokenService } from './../services/token.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const tokenService = inject(TokenService);
+
   return true;
 };
