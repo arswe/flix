1bd040103679133ca2aea4c0be31e952499c2b5c
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-17 17:40:32 +0600

    ReactiveFormsModule

--- frontend/src/app/pages/login/login.component.ts
+++ frontend/src/app/pages/login/login.component.ts
@@ -1,18 +1,16 @@
 import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { CardComponent } from '../../components/card/card.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, ReactiveFormsModule, FormControl, FormGroup],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  constructor(private toastrService: ToastrService) {}
-  openToast() {
-    this.toastrService.success('working');
-  }
+ 
 }
