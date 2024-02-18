361327fc08d4d81531a7d053f4ca8135da0bbb13
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 10:24:59 +0600

    Injectable

--- frontend/src/app/store/MessageState.ts
+++ frontend/src/app/store/MessageState.ts
@@ -1,5 +1,7 @@
-import { State } from '@ngxs/store';
+import { Injectable } from '@angular/core';
+import { Action, State } from '@ngxs/store';
 import { IMessage } from '../models/common.model';
+import { MessageService } from '../services/message.service';
 
 export class GetAllMessage {
   static readonly type = '[Message] Get All';
@@ -9,10 +11,15 @@ export interface MessageStateModel {
   messages: IMessage[] | undefined;
 }
 
-
 @State<MessageStateModel>({
   name: 'Message',
   defaults: {
     messages: [],
   },
 })
+@Injectable()
+export class MessageState {
+  constructor(private messageService: MessageService) {
+    @Action()
+  }
+}
