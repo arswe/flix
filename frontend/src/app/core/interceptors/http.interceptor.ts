847e55d6213bb877eb52cd4f8f824a9d1d7e05e7
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 09:52:52 +0600

    HttpInterceptorFn

--- frontend/src/app/core/interceptors/http.interceptor.ts
+++ frontend/src/app/core/interceptors/http.interceptor.ts
@@ -1,5 +1,8 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { TokenService } from './../services/token.service';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
+  const tokenService = inject(TokenService);
   return next(req);
 };
