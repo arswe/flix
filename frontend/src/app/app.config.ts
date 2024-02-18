1ae730828b52f69c796bb02b23c6a77dddb00ceb
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 10:00:52 +0600

    provideHttpClient

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,10 +1,15 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientModule,
+  provideHttpClient,
+  withInterceptors,
+} from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 import { NgxsModule } from '@ngxs/store';
 import { provideToastr } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { httpInterceptor } from './core/interceptors/http.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +21,6 @@ export const appConfig: ApplicationConfig = {
       preventDuplicates: true,
     }),
     importProvidersFrom(NgxsModule.forRoot(), HttpClientModule),
+    provideHttpClient(withInterceptors([httpInterceptor])),
   ],
 };
