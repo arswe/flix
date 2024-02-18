7411c09e9fb3e343134e9e2211773a0a018de278
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 09:58:14 +0600

    throwError

--- frontend/src/app/core/interceptors/http.interceptor.ts
+++ frontend/src/app/core/interceptors/http.interceptor.ts
@@ -1,6 +1,7 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { catchError, throwError } from 'rxjs';
 import { TokenService } from './../services/token.service';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
@@ -15,5 +16,13 @@ export const httpInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((e: HttpErrorResponse) => {
+      if (e.status === 401) {
+      }
+
+      const error = e.error?.message || e.statusText;
+      return throwError(() => error);
+    })
+  );
 };
