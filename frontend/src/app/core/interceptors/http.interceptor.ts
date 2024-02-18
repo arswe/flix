8514362073f29d907b4dddb1b968723d2e7b837a
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 09:54:45 +0600

    tokenService condition

--- frontend/src/app/core/interceptors/http.interceptor.ts
+++ frontend/src/app/core/interceptors/http.interceptor.ts
@@ -1,8 +1,15 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 import { TokenService } from './../services/token.service';
 
 export const httpInterceptor: HttpInterceptorFn = (req, next) => {
   const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  if (tokenService) {
+    req = req.clone({});
+  }
+
   return next(req);
 };
