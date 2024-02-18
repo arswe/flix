0306b744e07f8a8c5a4e0da14a8ea24ea6cd439c
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-18 10:23:06 +0600

    MessageStateModel

--- frontend/src/app/store/MessageState.ts
+++ frontend/src/app/store/MessageState.ts
@@ -1,3 +1,9 @@
+import { IMessage } from '../models/common.model';
+
 export class GetAllMessage {
   static readonly type = '[Message] Get All';
+}
+
+export interface MessageStateModel {
+  messages: IMessage[] | undefined;
 }
