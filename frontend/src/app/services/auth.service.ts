bddb1d8af6f7d476b4cc4112783ed8b84d73bf2c
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-19 14:17:01 +0600

    auth provider update

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
-
 import { apiEndpoint } from '../constants/constants';
 import { ILogin, ILoginResponse } from '../models/auth.model';
 import { TokenService } from './token.service';
@@ -17,7 +16,9 @@ export class AuthService {
       .post<ILoginResponse>(`${apiEndpoint.AuthEndpoint.login}`, data)
       .pipe(
         map((response) => {
+          console.log(response.data.token);
           if (response && response.data.token) {
+            console.log(response.data.token);
             this.tokenService.setToken(response.data.token);
           }
           return response;
