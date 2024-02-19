6d99281fe3e8b7b8b7af3f535c0dec40c5bb646e
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-19 13:58:50 +0600

    MessageComponent

--- frontend/src/app/pages/message/message.component.ts
+++ frontend/src/app/pages/message/message.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, OnInit } from '@angular/core';
 import { Select, Store } from '@ngxs/store';
 import { Observable } from 'rxjs';
@@ -11,33 +12,26 @@ import { GetAllMessage, MessageState } from '../../store/MessageState';
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [CardComponent, SidebarComponent, MessageItemComponent],
+  imports: [
+    CardComponent,
+    CommonModule,
+    SidebarComponent,
+    MessageItemComponent,
+  ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss',
 })
 export class MessageComponent implements OnInit {
-  @Select(MessageState.selectMessages) message$!: Observable<IMessage[]>;
-
+  @Select(MessageState.selectMessages) messages$!: Observable<IMessage[]>;
   constructor(private messageService: MessageService, private store: Store) {}
 
   ngOnInit(): void {
-    this.getAllMessages();
-
-    this.message$.subscribe({
+    this.messages$.subscribe({
       next: (value) => {
         if (!value.length) {
           this.store.dispatch(new GetAllMessage());
         }
-        console.log(value);
-      },
-    });
-  }
-
-  getAllMessages() {
-    this.messageService.getAllMessages().subscribe({
-      next(value) {
-        console.log(value);
       },
     });
   }
