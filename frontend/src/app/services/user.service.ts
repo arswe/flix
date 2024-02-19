a2558f6cb43b4ae54d5d9103c238821bcd376b98
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-19 13:35:33 +0600

    loggedInUser

--- frontend/src/app/services/user.service.ts
+++ frontend/src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { IApiResponse } from '../models/common.model';
+import { IUser } from '../models/auth.model';
+import { Observable } from 'rxjs';
+import { apiEndpoint } from '../constants/constants';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  onstructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
+
+  loggedInUser(): Observable<IApiResponse<IUser>> {
+    return this.http.get<IApiResponse<IUser>>(`${apiEndpoint.AuthEndpoint.me}`);
+  }
 }
