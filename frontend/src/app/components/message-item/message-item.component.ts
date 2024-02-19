94945cd5db5b9b802427ddb6f75cd68da1f24623
Abdur Rahman <abdurrahmansoftw@gmail.com>
2024-02-19 14:08:58 +0600

    DateAgoPipe

--- frontend/src/app/components/message-item/message-item.component.ts
+++ frontend/src/app/components/message-item/message-item.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { CUSTOM_ELEMENTS_SCHEMA, Component, Input } from '@angular/core';
 import { SidebarComponent } from '../../layouts/sidebar/sidebar.component';
 import { IMessage } from '../../models/common.model';
+import { DateAgoPipe } from '../../pipes/date-ago.pipe';
 import { CardComponent } from '../card/card.component';
 
 @Component({
@@ -12,6 +13,7 @@ import { CardComponent } from '../card/card.component';
     SidebarComponent,
     MessageItemComponent,
     CommonModule,
+    DateAgoPipe,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './message-item.component.html',
