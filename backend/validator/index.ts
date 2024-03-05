50ea32b94897b32445b65d3b803de2f278e8ae61
arswe <abdurrahmansoftw@gmail.com>
2024-03-05 11:13:02 +0600

    registerSchema

--- backend/validator/index.ts
+++ backend/validator/index.ts
@@ -10,11 +10,10 @@ export const loginSchema = z.object({
 
 export const registerSchema = z.object({
   name: z.string({ required_error: 'Name is required' }).min(3),
-
   email: z
     .string({ required_error: 'Email is required' })
     .email('Invalid Email Address'),
-    
+
   password: z.string({ required_error: 'Password is required' }).min(6),
 });
 
